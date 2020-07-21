2bdab04809b93c9a274fd102d1f7cb5212417315
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-07-21 13:50:18 -0400

    Added state

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState(false);
+  const [person, setPerson]= useState({name:'mario', age:40})
+
+  const clickHandler = () => {
+    setName(!name)
+    setPerson({name:"Luigi", age:10})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name ? "Taylor" : "Beth"}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  buttonContainer: {
+    marginTop:20,
+    backgroundColor:"blue",
+   
+  }
 });
