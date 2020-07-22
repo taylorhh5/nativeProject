9408777641cfc8b1b6262e040d28814d35abc41b
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-07-22 09:44:14 -0400

    Mapping through data

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState('taylor');
-  const [age, setAge]= useState("32")
+  const [name, setName] = useState("taylor");
+  const [age, setAge] = useState("32");
+
+  const [people, setPeople] = useState([
+    { name: "Leo", id: "1" },
+    { name: "Don", id: "2" },
+    { name: "Raph", id: "3" },
+    { name: "Mikey", id: "4" },
+  ]);
 
   // const clickHandler = () => {
   //   setName(!name)
@@ -16,23 +30,43 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Enter name:</Text>
-      <TextInput 
-      style={styles.input}
-      placeholder="e.g. John Doe" 
-      onChangeText={(val) => setName(val)} />
- <Text>Enter age:</Text>
-<TextInput 
-// keyboardAppearance="dark"
-keyboardType="numeric"
-      style={styles.input}
-      placeholder="e.g. 99" 
-      onChangeText={(val) => setAge(val)} />
-      
-      <Text>My name is {name} and my age is {age}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="e.g. John Doe"
+        onChangeText={(val) => setName(val)}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        keyboardAppearance="dark"
+        // keyboardType="numeric"
+        style={styles.input}
+        placeholder="e.g. 99"
+        onChangeText={(val) => setAge(val)}
+      />
+
+      <Text>
+        My name is {name} and my age is {age}
+      </Text>
       <View style={styles.buttonContainer}>
         {/* <Button title="update state" onPress={clickHandler} /> */}
       </View>
       <StatusBar style="auto" />
+
+      <FlatList
+        //keyExtractor since no key, ID instead
+        keyExtractor={(item) => item.id}
+        //data to cycle through
+        data={people}
+        //equal to a function that returns some JSX. Destructure item in array
+        renderItem={({ item }) => <Text style={styles.item}>{item.name} </Text>}
+      />
+
+      {/* {people.map(item => (
+        <View key={item.id}>
+
+<Text>{item.name} </Text>
+          </View>
+      ))} */}
     </View>
   );
 }
@@ -45,17 +79,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   buttonContainer: {
-    marginTop:20,
-    backgroundColor:"blue",
-   
+    marginTop: 20,
+    backgroundColor: "blue",
   },
 
-  input:{
-    borderWidth:1,
-    borderColor:"#777",
-    padding:8,
-    margin:10,
-    width:200,
-
-  }
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  item: {
+    width: 200,
+    marginBottom: 20,
+    backgroundColor: "pink",
+  },
 });
